f033656272f1ce00783e229522772433310f9881
fateh mohamed <cr7141414@gmail.com>
2020-11-08 19:06:15 +0100

    fix bugs

--- parceljsApp/src/currency.ts
+++ parceljsApp/src/currency.ts
@@ -5,20 +5,21 @@ import { CountryService } from "~service/country.service";
 console.log("CURRENCY module is lazy loaded....");
 const service = new CountryService();
 export default (val: string) => {
+  const searchMessage = document.getElementById("searchTitle") as HTMLElement;
+  const container = document.getElementsByClassName("country-container")[0];
+  container!.innerHTML = "<div></div>";
+  searchMessage.textContent = "🧐 Searching ...";
   return service.getCountriesByCurrency(val).pipe(
     tap((data) => {
       if (data.length) {
-        const container = document.getElementsByClassName(
-          "country-container"
-        )[0];
         const list = data.reduce((result: string, item: Country) => {
-          result += `<div class="country-item"><img src="${item.flag}"  alt="flag"/><h2> ${item?.name} </h2>  <small> ${item?.region}</small> <h2>Language: ${item?.languages[0].name}</h2></div>`;
+          result += `<div class="country-item"><img class="flag" src="${item.flag}"  alt="flag"/><div class="description"><h2> ${item?.name} </h2>  <small> ${item?.region}</small> <h2>Language: ${item?.languages[0].name}</h2></div></div>`;
           return result;
         }, "");
         container!.innerHTML = list;
-        (document.getElementById(
-          "searchTitle"
-        ) as HTMLElement).textContent = `Countries using ${data[0]?.currencies[0]?.code}`;
+        searchMessage.textContent = `Countries using ${data[0]?.currencies[0]?.code}`;
+      } else {
+        searchMessage.textContent = `No Countries found ☹️`;
       }
     })
   );
--- parceljsApp/src/language.ts
+++ parceljsApp/src/language.ts
@@ -5,12 +5,13 @@ import { CountryService } from "~service/country.service";
 console.log("LANGUAGE module is lazy loaded....");
 const service = new CountryService();
 export default (val: string) => {
+  const searchMessage = document.getElementById("searchTitle") as HTMLElement;
+  const container = document.getElementsByClassName("country-container")[0];
+  container!.innerHTML = "<div></div>";
+  searchMessage.textContent = "🧐 Searching ...";
   return service.getCountriesByLanguage(val).pipe(
     tap((data) => {
       if (data.length) {
-        const container = document.getElementsByClassName(
-          "country-container"
-        )[0];
         const list = data.reduce((result: string, item: Country) => {
           result += `<div class="country-item"><img class="flag" src="${item.flag}"  alt="flag"/><div class="description"><h2> ${item?.name} </h2>  <small> ${item?.region}</small> <h2>Language: ${item?.languages[0].name}</h2></div></div>`;
           return result;
@@ -19,6 +20,8 @@ export default (val: string) => {
         (document.getElementById(
           "searchTitle"
         ) as HTMLElement).textContent = `${data.length} Countries speaking ${data[0]?.languages[0]?.name}`;
+      } else {
+        searchMessage.textContent = `No Countries found ☹️`;
       }
     })
   );
